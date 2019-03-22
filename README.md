9ba5decf4e2618200d558eac12b063097fc68cb9
chas1ngwind <arsenalness@icloud.com>
2019-03-22 01:04:56 +0100

    final demo

--- README.md
+++ README.md
@@ -1,20 +1,37 @@
-# Information Visualization - Team 11
+## Welcome to GitHub Pages
 
-# Get start
-- python -m http.server 8888
-- open your browser: http://localhost:8888/
+You can use the [editor on GitHub](https://github.com/chas1ngwind/City-of-Amsterdam/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
 
-# Score
-### Population Stability Score
-- higher score means better population stability
-- PopulationStabilityScore = {[Normalized[min:0,max:1]:(people working/population)]*100} + {[Normalized[min:0,max:1]:(people moving to/people moving away)]*100}
+Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
 
-### Safety Score
-- higher score means less secure
-- Safety Score = Safety Index
+### Markdown
 
-### Living Condition Score:
-- higher score means better living condition
-- LivingConditionScore = PopulationStabilityScore - 0.5 * SafetyScore +100
-- Maxium LivingConditionScore is 120
-- Missing data is set to 0
+Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+
+```markdown
+Syntax highlighted code block
+
+# Header 1
+## Header 2
+### Header 3
+
+- Bulleted
+- List
+
+1. Numbered
+2. List
+
+**Bold** and _Italic_ and `Code` text
+
+[Link](url) and ![Image](src)
+```
+
+For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
+
+### Jekyll Themes
+
+Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/chas1ngwind/City-of-Amsterdam/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
+
+### Support or Contact
+
+Having trouble with Pages? Check out our [documentation](https://help.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
--- scripts/populationChart.js
+++ scripts/populationChart.js
@@ -13,32 +13,31 @@ var data = [],
       mini_width,
       textScale;
 
-	  var causes = ["Births", "Deaths", "Moving to","WithinAMS(to)","Moving away","WithinAMS(away)"]; 
+	  var causes = ["births", "deaths", "people_moving_to","moving_to_relocations_within_ams","people_moving_away","moving_away_relocations_within_ams"];
 
 var flagtest3=0;
 var updatePic3 = function () {
   flagtest3=flagtest3+1;
-  //console.log(flagtest3);
+  // console.log(flagtest2);
   return flagtest3;
 }
-  
+
   function populationChart(dist,year) {
-	  debugger
      ffff3=updatePic3();
   // console.log(ffff3);
-  
+
   if(ffff3>1){ 
     var  f = document.getElementById('divPopulation');
     var child = f.childNodes;
-    console.log(child);
+    // console.log(child);
     var i;
     var childs  = child.length;
-    for (i = 0; i < childs; i++) { 
+    for (i = 0; i < childs; i++) {
       f.removeChild(child[0]);
     }
-    
+
   };
-    
+
 	d3version3.csv("data/ams_stats_districts.csv",function(d){
 	 for(var y=0;y<d.length;y++){
       if(d[y].district==dist){
@@ -59,7 +58,7 @@ var updatePic3 = function () {
 				else if(i==4)
                  {my_object.value = parseInt(d[y].people_moving_away_2014);}
                 else if(i==5)
-                 {my_object.value = parseInt(d[y].people_moving_away_2014_relocations_within_ams);}				 
+                 {my_object.value = parseInt(d[y].people_moving_away_2014_relocations_within_ams);}
 				data.push(my_object);
                }
           }
@@ -82,7 +81,7 @@ var updatePic3 = function () {
                  {my_object.value = parseInt(d[y].people_moving_away_2015);}
                 else if(i==5)
                  {my_object.value = parseInt(d[y].people_moving_away_2015_relocations_within_ams);}
-                				 
+
 				data.push(my_object);
                }
         }
@@ -104,9 +103,9 @@ var updatePic3 = function () {
                  {my_object.value = parseInt(d[y].people_moving_away_2016);}
                 else if(i==5)
                  {my_object.value = parseInt(d[y].people_moving_away_2016_relocations_within_ams);}
-                				 
+
 				data.push(my_object);
-               } 
+               }
         }
         else if(parseInt(year)==2017)
         {
@@ -126,23 +125,24 @@ var updatePic3 = function () {
                  {my_object.value = parseInt(d[y].people_moving_away_2017);}
                 else if(i==5)
                  {my_object.value = parseInt(d[y].people_moving_away_2017_relocations_within_ams);}
-                				 
+
 				data.push(my_object);
                }
         }
+		else
+		{window.alert("Fail");}
       }
-	 } 
+	 }
 	 var zoomer = d3version3.behavior.zoom()
         .on("zoom", null);
 
-    var main_margin = {top: 35, right: 150, bottom: 70, left: 100},
-        main_width = 380 - main_margin.left - main_margin.right,
-        main_height = 250 - main_margin.top - main_margin.bottom;
+    var main_margin = {top: 10, right: 10, bottom: 30, left: 100},
+        main_width = 500 - main_margin.left - main_margin.right,
+        main_height = 400 - main_margin.top - main_margin.bottom;
 
-    var mini_margin = {top: 35, right: 5, bottom: 70, left: 5},
-	    mini_width = 76 - mini_margin.left - mini_margin.right;
-        mini_height = 250 - mini_margin.top - mini_margin.bottom;
-        
+    var mini_margin = {top: 10, right: 10, bottom: 30, left: 10},
+        mini_height = 400 - mini_margin.top - mini_margin.bottom;
+    mini_width = 100 - mini_margin.left - mini_margin.right;
 
     svg = d3version3.select("#divPopulation").append("svg")
         .attr("class", "svgWrapper")
@@ -158,22 +158,6 @@ var updatePic3 = function () {
         .on("touchstart.zoom", null)
         .on("touchmove.zoom", null)
         .on("touchend.zoom", null);
-	
-	svg.attr('class', 'headerText')
-          .append('text')
-          .attr("id", "titleBar")
-          .attr('transform', "translate(" + 150 + "," + 15 + ")")
-          .attr('text-anchor', 'middle')
-          .attr('font-weight', 600)
-          .text('Population Factors');
-	
-	svg.attr('class', 'headerText')
-          .append('text')
-          .attr("id", "titleBar")
-          .attr('transform', "translate(" + 150 + "," + 30 + ")")
-          .attr('text-anchor', 'middle')
-          .attr('font-weight', 200)
-          .text(dist + " in " + year);
 
     var mainGroup = svg.append("g")
             .attr("class","mainGroupWrapper")
@@ -191,12 +175,17 @@ var updatePic3 = function () {
             .attr("class","brushGroup")
             .attr("transform","translate(" + (main_margin.left + main_width + main_margin.right + mini_margin.left) + "," + mini_margin.top + ")");
 
+    /////////////////////////////////////////////////////////////
+    ////////////////////// Initiate scales //////////////////////
+    /////////////////////////////////////////////////////////////
+
     main_xScale = d3version3.scale.linear().range([0, main_width]);
     mini_xScale = d3version3.scale.linear().range([0, mini_width]);
 
     main_yScale = d3version3.scale.ordinal().rangeBands([0, main_height], 0.4, 0);
     mini_yScale = d3version3.scale.ordinal().rangeBands([0, mini_height], 0.4, 0);
 
+    //Based on the idea from: http://stackoverflow.com/questions/21485339/d3version3-brushing-on-grouped-bar-chart
     main_yZoom = d3version3.scale.linear()
         .range([0, main_height])
         .domain([0, main_height]);
@@ -227,32 +216,46 @@ var updatePic3 = function () {
         .attr("class", "y axis")
         .attr("transform", "translate(-5,0)");
 
+    /////////////////////////////////////////////////////////////
+    /////////////////////// Update scales ///////////////////////
+    /////////////////////////////////////////////////////////////
+
     //Update the scales
     main_xScale.domain([0, d3version3.max(data, function(d) { return d.value; })]);
     mini_xScale.domain([0, d3version3.max(data, function(d) { return d.value; })]);
     main_yScale.domain(data.map(function(d) { return d.country; }));
     mini_yScale.domain(data.map(function(d) { return d.country; }));
-    
+
     //Create the visual part of the y axis
     d3version3.select(".mainGroup").select(".y.axis").call(main_yAxis);
+
+    /////////////////////////////////////////////////////////////
+    ///////////////////// Label axis scales /////////////////////
+    /////////////////////////////////////////////////////////////
 
     textScale = d3version3.scale.linear()
       .domain([15,50])
       .range([12,6])
       .clamp(true);
-    
-    var brushExtent = 5;
+
+    /////////////////////////////////////////////////////////////
+    ///////////////////////// Create brush //////////////////////
+    /////////////////////////////////////////////////////////////
+
+    //What should the first extent of the brush become - a bit arbitrary this
+    var brushExtent = Math.max( 1, 5 );
 
     brush = d3version3.svg.brush()
         .y(mini_yScale)
         .extent([mini_yScale(data[0].country), mini_yScale(data[brushExtent].country)])
         .on("brush", brushmove)
+        //.on("brushend", brushend);
 
     //Set up the visual part of the brush
     gBrush = d3version3.select(".brushGroup").append("g")
       .attr("class", "brush")
       .call(brush);
-    
+
     gBrush.selectAll(".resize")
       .append("line")
       .attr("x2", mini_width);
@@ -260,8 +263,8 @@ var updatePic3 = function () {
     gBrush.selectAll(".resize")
       .append("path")
       .attr("d", d3version3.svg.symbol().type("triangle-up").size(20))
-      .attr("transform", function(d,i) { 
-        return i ? "translate(" + (mini_width/2) + "," + 4 + ") rotate(180)" : "translate(" + (mini_width/2) + "," + -4 + ") rotate(0)"; 
+      .attr("transform", function(d,i) {
+        return i ? "translate(" + (mini_width/2) + "," + 4 + ") rotate(180)" : "translate(" + (mini_width/2) + "," + -4 + ") rotate(0)";
       });
 
     gBrush.selectAll("rect")
@@ -272,7 +275,13 @@ var updatePic3 = function () {
       .on("mousedown.brush", brushcenter)
       .on("touchstart.brush", brushcenter);
 
+    ///////////////////////////////////////////////////////////////////////////
+    /////////////////// Create a rainbow gradient - for fun ///////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
     defs = svg.append("defs")
+
+    //Create two separate gradients for the main and mini bar - just because it looks fun
     createGradient("gradient-rainbow-main", "60%");
     createGradient("gradient-rainbow-mini", "13%");
 
@@ -284,6 +293,11 @@ var updatePic3 = function () {
       .attr("width", main_width + main_margin.left)
       .attr("height", main_height);
 
+    /////////////////////////////////////////////////////////////
+    /////////////// Set-up the mini bar chart ///////////////////
+    /////////////////////////////////////////////////////////////
+
+    //The mini brushable bar
     //DATA JOIN
     var mini_bar = d3version3.select(".miniGroup").selectAll(".bar")
       .data(data, function(d) { return d.key; });
@@ -311,11 +325,15 @@ var updatePic3 = function () {
     gBrush.call(brush.event);
 
   });
-   
+
   }//init
 
   //Function runs on a brush move - to update the big bar chart
   function update() {
+
+    /////////////////////////////////////////////////////////////
+    ////////// Update the bars of the main bar chart ////////////
+    /////////////////////////////////////////////////////////////
 
     //DATA JOIN
     var bar = d3version3.select(".mainGroup").selectAll(".bar")
@@ -344,6 +362,11 @@ var updatePic3 = function () {
       .remove();
 
   }//update
+
+  /////////////////////////////////////////////////////////////
+  ////////////////////// Brush functions //////////////////////
+  /////////////////////////////////////////////////////////////
+
   //First function that runs on a brush move
   function brushmove() {
 
@@ -351,7 +374,7 @@ var updatePic3 = function () {
 
     //Which bars are still "selected"
     var selected = mini_yScale.domain()
-      .filter(function(d) { return (extent[0] - mini_yScale.rangeBand() + 1e-2 <= mini_yScale(d)) && (mini_yScale(d) <= extent[1] - 1e-2); }); 
+      .filter(function(d) { return (extent[0] - mini_yScale.rangeBand() + 1e-2 <= mini_yScale(d)) && (mini_yScale(d) <= extent[1] - 1e-2); });
     //Update the colors of the mini chart - Make everything outside the brush grey
     d3version3.select(".miniGroup").selectAll(".bar")
       .style("fill", function(d, i) { return selected.indexOf(d.country) > -1 ? "url(#gradient-rainbow-mini)" : "#e0e0e0"; });
@@ -359,6 +382,10 @@ var updatePic3 = function () {
     //Update the label size
     d3version3.selectAll(".y.axis text")
       .style("font-size", textScale(selected.length));
+
+    /////////////////////////////////////////////////////////////
+    ///////////////////// Update the axes ///////////////////////
+    /////////////////////////////////////////////////////////////
 
     //Reset the part that is visible on the big chart
     var originalRange = main_yZoom.range();
@@ -385,9 +412,15 @@ var updatePic3 = function () {
 
     //Update the big bar chart
     update();
-    
+
   }//brushmove
 
+  /////////////////////////////////////////////////////////////
+  ////////////////////// Click functions //////////////////////
+  /////////////////////////////////////////////////////////////
+
+  //Based on http://bl.ocks.org/mbostock/6498000
+  //What to do when the user clicks on another location along the brushable bar chart
   function brushcenter() {
     var target = d3version3.event.target,
         extent = brush.extent(),
@@ -405,6 +438,10 @@ var updatePic3 = function () {
 
   }//brushcenter
 
+  /////////////////////////////////////////////////////////////
+  ///////////////////// Scroll functions //////////////////////
+  /////////////////////////////////////////////////////////////
+
   function scroll() {
 
     //Mouse scroll on the mini chart
@@ -416,8 +453,8 @@ var updatePic3 = function () {
       dy = d3version3.event.deltaY,
       topSection;
 
-    if ( extent[0] - dy < y0 ) { topSection = y0; } 
-    else if ( extent[1] - dy > y1 ) { topSection = y1 - size; } 
+    if ( extent[0] - dy < y0 ) { topSection = y0; }
+    else if ( extent[1] - dy > y1 ) { topSection = y1 - size; }
     else { topSection = extent[0] - dy; }
 
     //Make sure the page doesn't scroll as well
@@ -430,6 +467,11 @@ var updatePic3 = function () {
 
   }//scroll
 
+  /////////////////////////////////////////////////////////////
+  ///////////////////// Helper functions //////////////////////
+  /////////////////////////////////////////////////////////////
+
+  //Create a gradient
   function createGradient(idName, endPerc) {
 
     var coloursRainbow = ["#EFB605", "#E9A501", "#E48405", "#E34914", "#DE0D2B", "#CF003E", "#B90050", "#A30F65", "#8E297E", "#724097", "#4F54A8", "#296DA4", "#0C8B8C", "#0DA471", "#39B15E", "#7EB852"];
@@ -439,9 +481,11 @@ var updatePic3 = function () {
       .attr("gradientUnits", "userSpaceOnUse")
       .attr("x1", "0%").attr("y1", "0%")
       .attr("x2", endPerc).attr("y2", "0%")
-      .selectAll("stop") 
-      .data(coloursRainbow)                  
-      .enter().append("stop") 
-      .attr("offset", function(d,i) { return i/(coloursRainbow.length-1); })   
+      .selectAll("stop")
+      .data(coloursRainbow)
+      .enter().append("stop")
+      .attr("offset", function(d,i) { return i/(coloursRainbow.length-1); })
       .attr("stop-color", function(d) { return d; });
   }//createGradient
+
+  //init("Houthavens",2014);
